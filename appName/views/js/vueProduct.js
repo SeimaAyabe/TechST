ba0257cc687af43d94ca46a805a9b611a1f4752e
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-07-24 19:10:58 +0900

    experiment

--- appName/views/js/vueProduct.js
+++ appName/views/js/vueProduct.js
@@ -13,10 +13,18 @@ new Vue({
         // 商品情報の状態
         current: -1,
         // 商品情報の状態一覧
-        options: [
-            { value: -1, label: 'すべて' },
-            { value:  0, label: '未購入' },
-            { value:  1, label: '購入済' }
+        options: [{
+                value: -1,
+                label: 'すべて'
+            },
+            {
+                value: 0,
+                label: '未購入'
+            },
+            {
+                value: 1,
+                label: '購入済'
+            }
         ],
         // true：入力済・false：未入力
         isEntered: false
@@ -27,15 +35,17 @@ new Vue({
         // 商品情報の状態一覧を表示する
         labels() {
             return this.options.reduce(function (a, b) {
-                return Object.assign(a, { [b.value]: b.label })
+                return Object.assign(a, {
+                    [b.value]: b.label
+                })
             }, {})
         },
         // 表示対象の商品情報を返却する
         computedProducts() {
-          return this.products.filter(function (el) {
-            var option = this.current < 0 ? true : this.current === el.state
-            return option
-          }, this)
+            return this.products.filter(function (el) {
+                var option = this.current < 0 ? true : this.current === el.state
+                return option
+            }, this)
         },
         // 入力チェック
         validate() {
@@ -46,7 +56,7 @@ new Vue({
     },
 
     // インスタンス作成時の処理
-    created: function() {
+    created: function () {
         this.doFetchAllProducts()
     },
 
@@ -55,37 +65,37 @@ new Vue({
         // 全ての商品情報を取得する
         doFetchAllProducts() {
             axios.get('/fetchAllProducts')
-            .then(response => {
-                if (response.status != 200) {
-                    throw new Error('レスポンスエラー')
-                } else {
-                    var resultProducts = response.data
+                .then(response => {
+                    if (response.status != 200) {
+                        throw new Error('レスポンスエラー')
+                    } else {
+                        var resultProducts = response.data
 
-                    // サーバから取得した商品情報をdataに設定する
-                    this.products = resultProducts
-                }
-            })
+                        // サーバから取得した商品情報をdataに設定する
+                        this.products = resultProducts
+                    }
+                })
         },
         // １つの商品情報を取得する
         doFetchProduct(product) {
             axios.get('/fetchProduct', {
-                params: {
-                    productID: product.id
-                }
-            })
-            .then(response => {
-                if (response.status != 200) {
-                    throw new Error('レスポンスエラー')
-                } else {
-                    var resultProduct = response.data
+                    params: {
+                        productID: product.id
+                    }
+                })
+                .then(response => {
+                    if (response.status != 200) {
+                        throw new Error('レスポンスエラー')
+                    } else {
+                        var resultProduct = response.data
 
-                    // 選択された商品情報のインデックスを取得する
-                    var index = this.products.indexOf(product)
+                        // 選択された商品情報のインデックスを取得する
+                        var index = this.products.indexOf(product)
 
-                    // spliceを使うとdataプロパティの配列の要素をリアクティブに変更できる
-                    this.products.splice(index, 1, resultProduct[0])
-                }
-            })
+                        // spliceを使うとdataプロパティの配列の要素をリアクティブに変更できる
+                        this.products.splice(index, 1, resultProduct[0])
+                    }
+                })
         },
         // 商品情報を登録する
         doAddProduct() {
@@ -95,17 +105,17 @@ new Vue({
             params.append('productMemo', this.productMemo)
 
             axios.post('/addProduct', params)
-            .then(response => {
-                if (response.status != 200) {
-                    throw new Error('レスポンスエラー')
-                } else {
-                    // 商品情報を取得する
-                    this.doFetchAllProducts()
+                .then(response => {
+                    if (response.status != 200) {
+                        throw new Error('レスポンスエラー')
+                    } else {
+                        // 商品情報を取得する
+                        this.doFetchAllProducts()
 
-                    // 入力値を初期化する
-                    this.initInputValue()
-                }
-            })
+                        // 入力値を初期化する
+                        this.initInputValue()
+                    }
+                })
         },
         // 商品情報の状態を変更する
         doChangeProductState(product) {
@@ -115,14 +125,14 @@ new Vue({
             params.append('productState', product.state)
 
             axios.post('/changeStateProduct', params)
-            .then(response => {
-                if (response.status != 200) {
-                    throw new Error('レスポンスエラー')
-                } else {
-                    // 商品情報を取得する
-                    this.doFetchProduct(product)
-                }
-            })
+                .then(response => {
+                    if (response.status != 200) {
+                        throw new Error('レスポンスエラー')
+                    } else {
+                        // 商品情報を取得する
+                        this.doFetchProduct(product)
+                    }
+                })
         },
         // 商品情報を削除する
         doDeleteProduct(product) {
@@ -131,14 +141,14 @@ new Vue({
             params.append('productID', product.id)
 
             axios.post('/deleteProduct', params)
-            .then(response => {
-                if (response.status != 200) {
-                    throw new Error('レスポンスエラー')
-                } else {
-                    // 商品情報を取得する
-                    this.doFetchAllProducts()
-                }
-            })
+                .then(response => {
+                    if (response.status != 200) {
+                        throw new Error('レスポンスエラー')
+                    } else {
+                        // 商品情報を取得する
+                        this.doFetchAllProducts()
+                    }
+                })
         },
         // 入力値を初期化する
         initInputValue() {
@@ -148,4 +158,3 @@ new Vue({
         }
     }
 })
-
