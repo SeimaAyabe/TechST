6e3c52e28cc0d05f406f698afdc2396aad7a32fa
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-07-26 19:49:36 +0900

    date which are in a week

--- appName/views/js/modal.js
+++ appName/views/js/modal.js
@@ -1,5 +1,16 @@
 $(function () {
     $('.js-modal-open').on('click', function () {
+        var now = new Date();
+        var wd = ['日', '月', '火', '水', '木', '金', '土'];
+
+        $('.dateSlideList li').text(function () {
+            var m = now.getMonth() + 1;
+            var d = now.getDate();
+            var w = wd[now.getDay()];
+            now.setDate(now.getDate() + 1);
+            return m + "月" + d + "日" + "(" + w + ")";
+        });
+
         $('.js-modal').fadeIn();
         return false;
     });
