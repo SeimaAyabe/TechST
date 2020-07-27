417e7376e3bfed354171e8ca48a9354933880e6a
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-07-27 20:29:10 +0900

    selectbox about a day

--- appName/views/js/modal.js
+++ appName/views/js/modal.js
@@ -2,14 +2,16 @@ $(function () {
     $('.js-modal-open').on('click', function () {
         var now = new Date();
         var wd = ['日', '月', '火', '水', '木', '金', '土'];
+        var selectValue = 1;
 
-        $('.dateSlideList li').text(function () {
+        for (var i = 0; i < 7; i++) {
             var m = now.getMonth() + 1;
             var d = now.getDate();
             var w = wd[now.getDay()];
             now.setDate(now.getDate() + 1);
-            return m + "月" + d + "日" + "(" + w + ")";
-        });
+            $('.dateSlide').append($('<option>').html(m + "月" + d + "日" + "(" + w + ")").val(selectValue));
+            selectValue++;
+        }
 
         $('.js-modal').fadeIn();
         return false;
