6997ce8bf88ebfda21be62b4be06e83ff33dbbc3
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-12-06 17:56:15 +0900

    add comment

--- sampleEC/models/dao/productDao.go
+++ sampleEC/models/dao/productDao.go
@@ -2,9 +2,8 @@ package dao
 
 import ( // フォーマットI/O
 	// GORM (Go言語のORM)
-	// DB共通処理
-	"fmt"
 
+	// DB共通処理
 	dbcommonlogic "github.com/username/sampleEC/models/dbcommonlogic"
 
 	// エンティティアクセル用モジュール
@@ -36,7 +35,6 @@ func SearchProducts(searchedProducts string) []entity.Product {
 	// DB内で検索キーワードの部分一致検索を行う
 	db.Where("name LIKE ?", "%"+searchedProducts+"%").Find(&product)
 
-	fmt.Println(product)
 	// DB切断 (return時に実行)
 	defer db.Close()
 
--- sampleEC/services/productService.go
+++ sampleEC/services/productService.go
@@ -10,21 +10,24 @@ import (
 
 // CompilePrice は 商品の値段を表示させる為の処理
 func CompilePrice(productList []entity.Product) {
-
-	fmt.Println(productList[0].Price)
-
+	// 数値を文字列に変換する
 	stringPrice := strconv.Itoa(productList[0].Price)
 
-	fmt.Println(len(stringPrice))
-
+	// カンマ入りの値段を表示させる為の文字列を定義
 	var commaPrice string
 
+	// カンマの入れる位置を判断する為の数値を定義
 	var priceCount int
 
+	// 文字列の長さ分、処理を繰り返す
 	for i := len(stringPrice); i > 0; i-- {
-
+		// 文字列の後ろから文字を代入していく
 		commaPrice += stringPrice[i-1 : i]
+
+		// 数値をインクリメント
 		priceCount++
+
+		// 次はここからスタート
 		if priceCount%3 == 0 && priceCount != len(stringPrice) {
 			commaPrice += ","
 		}
