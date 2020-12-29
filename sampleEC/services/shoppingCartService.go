a39369b4e0c01738f6d9372ebf9a8a3c418e7676
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-12-29 20:05:59 +0900

    introduce add quantity function

--- sampleEC/services/shoppingCartService.go
+++ sampleEC/services/shoppingCartService.go
@@ -3,6 +3,7 @@ package services
 import (
 
 	// エンティティアクセル用モジュール
+
 	"fmt"
 	"strconv"
 
@@ -17,9 +18,15 @@ func JudgeShoppingCart(productID string, quantity []string) {
 
 	// 買い物カゴ内のデータを１つずつチェックし、選択した商品が既に入っているかどうかを確認する
 	for i := 0; i < len(getShoppingCarts); i++ {
+		// 既に商品が入っていた場合、加算する
 		if productID == strconv.Itoa(getShoppingCarts[i].ProductID) {
+			// 文字列を数値に変換
+			compiledQuantity, _ := strconv.Atoi(quantity[0])
 
-			fmt.Println(productID)
+			// 加算処理
+			addedQuantity := getShoppingCarts[i].Quantity + compiledQuantity
+
+			fmt.Println(addedQuantity)
 		}
 	}
 
